13f3ed8853a899b75c09c645ad4afec5277ba652
buyixiaojiang <chengtingsen@163.com>
2017-02-26 15:42:54 +0800

    通过观察者订阅模式来实现$watch函数。有个问题就是key相同的时候，触发的同一个事件

---
+++ nuomi/js/pubsub.js
@@ -0,0 +1,32 @@
+function Pubsub() {
+    this.handlers = {};
+}
+
+Pubsub.prototype = {
+    on: function (eventType, handler) {
+        var self = this;
+        if ( !(eventType in self.handlers) ) {
+            self.handlers[eventType] = [];
+        }
+        self.handlers[eventType].push(handler);
+        return this;
+    },
+    emit: function (eventType) {
+        var length = this.handlers[eventType].length;
+        var args = Array.prototype.slice.call(arguments, 1);
+        for (var i = 0; i < length; i++) {
+            this.handlers[eventType][i].apply(this, args);
+        }
+        return this;
+    }
+}
+
+var pubSub = new Pubsub();
+pubSub.on('A', function (data) {
+    console.log(1 + data);
+});
+pubSub.on('A', function (data) {
+    console.log(2 + data);
+});
+pubSub.emit('A', '我是参数');
+
